d7bcfa90c45df596e0d665c0046227cd8f98521c
Yana Belkina <tosja013@gmail.com>
2021-03-19 21:21:03 +0300

    Add finnhab api

--- YA.STOCK/YA_STOCK.swift
+++ YA.STOCK/YA_STOCK.swift
@@ -38,6 +38,34 @@ struct YA_STOCKApp: App {
         
         dataTask.resume()
     }
+    
+//    init(){
+//        let headers = [
+//            "X-Finnhub-Token": "c1ae1bn48v6ulhh7kbrg"
+//        ]
+//
+//        let request = NSMutableURLRequest(
+//            url: NSURL(string: "https://finnhub.io/api/v1/stock?token=")! as URL,
+//            cachePolicy: .useProtocolCachePolicy,
+//            timeoutInterval: 10.0)
+//        request.httpMethod = "GET"
+//        request.allHTTPHeaderFields = headers
+//
+//        let session = URLSession.shared
+//        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+//            if (error != nil) {
+//                print(error!)
+//            } else {
+//                let httpResponse = response as? HTTPURLResponse
+//                print(httpResponse!)
+//                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+//                    print("Response data string:\n \(dataString)")
+//                }
+//            }
+//        })
+//
+//        dataTask.resume()
+//    }
 
     var body: some Scene {
         WindowGroup {
